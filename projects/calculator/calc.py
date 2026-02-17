b2fc720f1aba4eb8c6df89310f84fe05a241b20b
Mayank Bijjal <mayank.bijjal@netcracker.com>
2026-02-17 10:47:47 +0530

    sub -20

--- projects/calculator/calc.py
+++ projects/calculator/calc.py
@@ -5,7 +5,6 @@ class Calculator:
     
     def add(self):
         return self.num1+self.num2
-    def sub(self):     
-        a =  self.num1-self.num2
-        return a
+    def sub(self):
+        return self.num1-self.num2 -20
     
